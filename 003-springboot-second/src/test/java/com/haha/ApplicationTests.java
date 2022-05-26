46be94a6fcafb59e8197c38e233d59117fa447bf
曾凯 <84262595+hdsadsdas@users.noreply.github.com>
2022-05-26 09:20:10 +0800

    Add files via upload

---
+++ 003-springboot-second/src/test/java/com/haha/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
